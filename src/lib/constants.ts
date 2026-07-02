ef742658c94650d2f7997b97b32beb6fb85c6062
FoundingEngineer <foundingengineer@sixzenith.com>
2026-07-02 09:00:24 +0000

    fix: update lg6 whatsapp destinations

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,6 @@
-export const CTA_URL = 'https://szfunnel.zenova.id/api/wa/52c7314c-e9b8-406e-aa5a-690d5e83afbb/lg6';
+export const PRIORITY_1_CTA_URL =
+  'https://zenichat.id/api/wa/52c7314c-e9b8-406e-aa5a-690d5e83afbb/lg6';
+
+export const PRIORITY_2_CTA_URL = 'https://wa.me/6282136880159';
+
+export const CTA_URL = PRIORITY_1_CTA_URL;
